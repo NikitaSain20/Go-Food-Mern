1b99595166d52da322517f6e5f645badab32df30
nikita <nikitasen664@gmail.com>
2025-08-07 20:17:10 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(
 );
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "process.env.FRONTEND_URL");
+  res.setHeader("Access-Control-Allow-Origin", process.env.FRONTEND_URL);
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
