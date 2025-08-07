199a5e22b30461089255d2d020717d2a4166827c
nikita <nikitasen664@gmail.com>
2025-08-07 21:34:09 +0530

    changes done in my order

--- frontend/src/screen/MyOrder.js
+++ frontend/src/screen/MyOrder.js
@@ -34,69 +34,60 @@ export default function MyOrder() {
 
       <div className="container">
         <div className="row">
-          {orderData !== {}
-            ? Array(orderData).map((data) => {
-                return data.orderData
-                  ? data.orderData.order_data
-                      .slice(0)
-                      .reverse()
-                      .map((item) => {
-                        return item.map((arrayData) => {
+          {orderData &&
+            Array.isArray(orderData) &&
+            orderData.map(
+              (data, index) =>
+                data.orderData?.order_data &&
+                data.orderData.order_data
+                  .slice(0)
+                  .reverse()
+                  .map((items, i) => (
+                    <React.Fragment key={`${index}-${i}`}>
+                      {items.map((arrayData, j) => {
+                        if (arrayData.Order_date) {
                           return (
-                            <div>
-                              {arrayData.Order_date ? (
-                                <div className="m-auto mt-5">
-                                  {(data = arrayData.Order_date)}
-                                  <hr />
-                                </div>
-                              ) : (
-                                <div className="col-12 col-md-6 col-lg-3">
-                                  <div
-                                    className="card mt-3"
-                                    style={{
-                                      width: "16rem",
-                                      maxHeight: "360px",
-                                    }}
-                                  >
-                                    <img
-                                      src={arrayData.img}
-                                      className="card-img-top"
-                                      alt="..."
-                                      style={{
-                                        height: "120px",
-                                        objectFit: "fill",
-                                      }}
-                                    />
-                                    <div className="card-body">
-                                      <h5 className="card-title">
-                                        {arrayData.name}
-                                      </h5>
-                                      <div
-                                        className="container w-100 p-0"
-                                        style={{ height: "38px" }}
-                                      >
-                                        <span className="m-1">
-                                          {arrayData.qty}
-                                        </span>
-                                        <span className="m-1">
-                                          {arrayData.size}
-                                        </span>
-                                        <span className="m-1">{data}</span>
-                                        <div className=" d-inline ms-2 h-100 w-20 fs-5">
-                                          ₹{arrayData.price}/-
-                                        </div>
-                                      </div>
-                                    </div>
-                                  </div>
-                                </div>
-                              )}
+                            <div
+                              className="col-12 mt-5"
+                              key={`date-${index}-${i}-${j}`}
+                            >
+                              <h5 className="text-center">
+                                {arrayData.Order_date}
+                              </h5>
+                              <hr />
                             </div>
                           );
-                        });
-                      })
-                  : "";
-              })
-            : ""}
+                        } else {
+                          return (
+                            <div
+                              className="col-12 col-md-6 col-lg-3"
+                              key={`card-${index}-${i}-${j}`}
+                            >
+                              <div
+                                className="card mt-3"
+                                style={{ width: "16rem", maxHeight: "360px" }}
+                              >
+                                <div className="card-body">
+                                  <h5 className="card-title">
+                                    {arrayData.name}
+                                  </h5>
+                                  <div
+                                    className="container w-100 p-0 d-flex justify-content-between align-items-center"
+                                    style={{ height: "38px" }}
+                                  >
+                                    <span>{arrayData.qty}</span>
+                                    <span>{arrayData.size}</span>
+                                    <span>₹{arrayData.price}/-</span>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          );
+                        }
+                      })}
+                    </React.Fragment>
+                  ))
+            )}
         </div>
       </div>
 
@@ -104,4 +95,3 @@ export default function MyOrder() {
     </div>
   );
 }
-// {"orderData":{"_id":"63024fd2be92d0469bd9e31a","email":"mohanDas@gmail.com","order_data":[[[{"id":"62ff20fbaed6a15f800125e9","name":"Chicken Fried Rice","qty":"4","size":"half","price":520},{"id":"62ff20fbaed6a15f800125ea","name":"Veg Fried Rice","qty":"4","size":"half","price":440}],"2022-08-21T15:31:30.239Z"],[[{"id":"62ff20fbaed6a15f800125f4","name":"Mix Veg Pizza","qty":"4","size":"medium","price":800},{"id":"62ff20fbaed6a15f800125f3","name":"Chicken Doub;e Cheeze Pizza","qty":"4","size":"regular","price":480}],"2022-08-21T15:32:38.861Z"]],"__v":0}}
