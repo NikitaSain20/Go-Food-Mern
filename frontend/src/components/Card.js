1657c30fc0b76aac448cffc9414d814b7e3d4ab0
nikita <nikitasen664@gmail.com>
2025-08-12 21:00:12 +0530

    changes to be done

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import { useCart, useDispatchCart } from "./CardReducer";
+
 export default function Card(props) {
   const dispatch = useDispatchCart();
   let data = useCart();
@@ -8,14 +9,16 @@ export default function Card(props) {
   let priceOptions = Object.keys(Options);
   const [qty, setQty] = useState(1);
   const [size, setSize] = useState("");
-  // let foodItem = props.foodItems;
+  const [showMessage, setShowMessage] = useState(false); // For toast message
+
+  const finalPrice = qty * parseInt(Options[size]);
+
   const handleAddToCart = async () => {
-    alert("please logoin first");
+    // Your existing add-to-cart logic
     let food = [];
     for (const item of data) {
       if (item.id === props.foodItem._id) {
         food = item;
-
         break;
       }
     }
@@ -27,7 +30,6 @@ export default function Card(props) {
           price: finalPrice,
           qty: qty,
         });
-        return;
       } else if (food.size !== size) {
         await dispatch({
           type: "ADD",
@@ -38,91 +40,96 @@ export default function Card(props) {
           size: size,
           img: props.ImgSrc,
         });
-        console.log("Size different so simply ADD one more to the list");
-        return;
       }
-      return;
+    } else {
+      await dispatch({
+        type: "ADD",
+        id: props.foodItem._id,
+        name: props.foodItem.name,
+        price: finalPrice,
+        qty: qty,
+        size: size,
+      });
     }
 
-    await dispatch({
-      type: "ADD",
-      id: props.foodItem._id,
-      name: props.foodItem.name,
-      price: finalPrice,
-      qty: qty,
-      size: size,
-    });
-
-    // setBtnEnable(true)
+    // Show success message
+    setShowMessage(true);
+    setTimeout(() => setShowMessage(false), 1500);
   };
 
-  // setBtnEnable(true)
-  const finalPrice = qty * parseInt(Options[size]);
   useEffect(() => {
     setSize(priceRef.current.value);
   }, []);
+
   return (
-    <div>
-      {" "}
-      <div className="container">
-        <div
-          className="card my-5 mx-4"
-          style={{ width: "18rem", minHeight: "380px" }}
-        >
-          <img
-            style={{ height: "170px", objectFit: "fill" }}
-            src={props.foodItem.img}
-            class="card-img-top "
-            alt="..."
-          />
-          <div class="card-body">
-            <h5 class="card-title">{props.foodItem.name}</h5>
-            <p class="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <div className="container w-100 ">
-              <select
-                className="m-2 h-100 bg-success rounded"
-                onChange={(e) => {
-                  setQty(e.target.value);
-                }}
-              >
-                {Array.from(Array(6), (e, i) => {
-                  return (
-                    <option key={i + 1} value={i + 1}>
-                      {i + 1}
-                    </option>
-                  );
-                })}
-              </select>
-              <select
-                className="m-2 h-100 bg-success rounded"
-                ref={priceRef}
-                onChange={(e) => {
-                  setSize(e.target.value);
-                }}
-              >
-                {priceOptions.map((data) => {
-                  return (
-                    <option key={data} value={data}>
-                      {data}
-                    </option>
-                  );
-                })}
-                {/* <option value="half">Half</option>
-                <option value="full">Full</option> */}
-              </select>
-              <div className="d-inline fs-5">Rs{finalPrice}/-</div>
-              <hr className="text-dark fw-bolder"></hr>
-              <div className="d-flex flex-row">
-                <button className="btn btn-danger" onClick={handleAddToCart}>
-                  ADD TO CART
-                </button>
-              </div>
+    <div className="container">
+      <div
+        className="card my-5 mx-4"
+        style={{ width: "18rem", minHeight: "380px", position: "relative" }}
+      >
+        <img
+          style={{ height: "170px", objectFit: "fill" }}
+          src={props.foodItem.img}
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">{props.foodItem.name}</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <div className="container w-100">
+            <select
+              className="m-2 h-100 bg-success rounded"
+              onChange={(e) => setQty(e.target.value)}
+            >
+              {Array.from(Array(6), (e, i) => (
+                <option key={i + 1} value={i + 1}>
+                  {i + 1}
+                </option>
+              ))}
+            </select>
+            <select
+              className="m-2 h-100 bg-success rounded"
+              ref={priceRef}
+              onChange={(e) => setSize(e.target.value)}
+            >
+              {priceOptions.map((data) => (
+                <option key={data} value={data}>
+                  {data}
+                </option>
+              ))}
+            </select>
+            <div className="d-inline fs-5">Rs{finalPrice}/-</div>
+            <hr className="text-dark fw-bolder"></hr>
+            <div className="d-flex flex-row">
+              <button className="btn btn-danger" onClick={handleAddToCart}>
+                ADD TO CART
+              </button>
             </div>
           </div>
         </div>
+
+        {/* Success Toast */}
+        {showMessage && (
+          <div
+            style={{
+              position: "absolute",
+              bottom: "10px",
+              left: "50%",
+              transform: "translateX(-50%)",
+              background: "#d4edda",
+              color: "#155724",
+              padding: "6px 12px",
+              borderRadius: "4px",
+              fontSize: "14px",
+              boxShadow: "0px 2px 5px rgba(0,0,0,0.2)",
+            }}
+          >
+            ✅ Item added to cart
+          </div>
+        )}
       </div>
     </div>
   );
