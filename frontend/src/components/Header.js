ea75ffab801f0b97cd95d17067718e7d6e7db5a7
nikita <nikitasen664@gmail.com>
2025-08-12 21:42:08 +0530

    chnages

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -87,7 +87,7 @@ export default function Header() {
                   <i class="bi bi-cart-dash-fill f-3 position-relative"></i>
                   {/* <ShoppingCarzzt /> */}
                   {/* <Badge bg="danger" className="ms-2"> */}
-                  <span className="text-success fs-5 position-absolute top-0 mt-1">
+                  <span className="text-success fs-5 position-absolute top-0 mt-2">
                     {data.length}
                   </span>
                   {/* </Badge> */}
